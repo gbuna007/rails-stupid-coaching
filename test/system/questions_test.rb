a10b87c83cdb88a0688b2dca680abbe91c5ad849
Grace Bunardi <gbunardi@hotmail.com>
2022-08-16 11:32:58 +0800

    testing done

---
+++ test/system/questions_test.rb
@@ -0,0 +1,19 @@
+require "application_system_test_case"
+
+class QuestionsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit ask_url
+  #   assert_selector "p", text: "Ask your coach anything"
+  # end
+
+  test "saying Hello yields a grumpy response from the coach" do
+    visit ask_url
+    fill_in "ask", with: "Hello"
+    click_on "Ask!"
+
+    assert_text "I don't care, get dressed and go to work!"
+    take_screenshot
+  end
+
+
+end
